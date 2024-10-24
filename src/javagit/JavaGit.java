dd62f7d01c0a4c19f5b70270828742b36faec686
fabuandras <fabuandras@gmail.com>
2024-10-24 16:03:03 +0200

    refact: termális operátorral

--- src/javagit/JavaGit.java
+++ src/javagit/JavaGit.java
@@ -13,7 +13,7 @@ public class JavaGit {
         final int HIDEG_HATAR = 10;
         int homerseklet = 7;
         boolean hidegVan = homerseklet < HIDEG_HATAR;
-        String eredmeny = "Hideg van, viszek kabátot.";
+        String eredmeny = "Viszek kabátot.";
         
         /* termális operátorral: */
         eredmeny = hidegVan ? eredmeny : "Nem viszek kabátot. " + eredmeny;
