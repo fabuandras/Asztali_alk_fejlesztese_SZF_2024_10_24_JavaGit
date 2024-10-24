2ee257f75cdf69492a323f8c19042eef2c635562
fabuandras <fabuandras@gmail.com>
2024-10-24 15:55:22 +0200

    feladat megoldása: hagyományos if/else szerkezettel

--- src/javagit/JavaGit.java
+++ src/javagit/JavaGit.java
@@ -3,7 +3,24 @@ package javagit;
 public class JavaGit {
 
     public static void main(String[] args) {
+        /* 10 fok alatt van hideg
+            ha hideg van, akkor
+              viszek kabátot
+            egyébként
+              nem viszek kabátot
+        */
         
+        int homerseklet = 5;
+
+        String eredmeny = "";
+        
+        if (homerseklet < 10) {
+            eredmeny = "Hideg van, viszek kabátot.";
+        } else {
+            eredmeny = "Nem viszek kabátot.";
+        }
+        
+        System.out.println(eredmeny);
     }
     
 }
